c95b19eb0a8149ecbb0b2b1aea6a627fe245aa22
adash333 <adash333@hotmail.com>
2018-06-09 10:40:07 +0900

    use ngFor for comments

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Comment } from '../../models/chat';
+
+const COMMENTS: Comment[] = [
+  { name: "Suzuki Taro", content: "1つ目のコメントです。"},
+  { name: "Suzuki Taro", content: "2つ目のコメントです。"},
+  { name: "Suzuki Taro", content: "3つ目のコメントです。"}
+];
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  public content = '';
+  public comments = COMMENTS;
 
   constructor(public navCtrl: NavController) {
 
