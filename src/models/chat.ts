572f40102ab29991926b44e575d086b7f4184339
adash333 <adash333@hotmail.com>
2018-06-09 22:14:20 +0900

    modity datetime  with angular datepipe

--- src/models/chat.ts
+++ src/models/chat.ts
@@ -12,10 +12,12 @@ export class Comment {
   user: User;
   initial: string;
   content: string;
+  date: number;
 
   constructor(user: User, content: string) {
     this.user = user;
     this.initial = user.name.slice(0, 1);
     this.content = content;
+    this.date = Date.now();
   }
 }
