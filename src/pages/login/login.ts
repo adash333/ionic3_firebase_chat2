d4206d6804721f04c980c168fa6e01c4791932e5
adash333 <adash333@hotmail.com>
2018-06-12 12:00:16 +0900

    test rxjs/Observable

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { SignUpPage } from '../sign-up/sign-up';
 
+import { Observable } from 'rxjs/Observable';
+
 @IonicPage()
 @Component({
   selector: 'page-login',
@@ -10,11 +12,21 @@ import { SignUpPage } from '../sign-up/sign-up';
 })
 export class LoginPage {
 
+  public loginObservable: Observable<string> = new Observable((observer) => {
+    observer.next('text1');
+    setTimeout(() => {
+      observer.next('text2');
+    }, 5000)
+  })
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+    this.loginObservable.subscribe((data: string) => {
+      console.log(data);
+    })
   }
 
   goToSignUpPage() {
